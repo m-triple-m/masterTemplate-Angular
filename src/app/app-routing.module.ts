e011a4e5efcead34d03ab1706a2c3239ec8351fe
Mohammad-Mubassir <triplem656@gmail.com>
2021-04-15 11:09:37 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { LayoutComponent as AdminLayout } from './admin/layout/layout.component';
+import { LayoutComponent as UserLayout } from './user/layout/layout.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/blog', pathMatch: 'full' },
+
+  {
+    path: 'admin',
+    component: AdminLayout,
+    children: [{ path: 'dashboard', component: DashboardComponent }],
+  },
+  {
+    path: 'user',
+    component: UserLayout,
+    children: [],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
